c27f50741f972ccb69f770105d48050acb530aed
Mohit-S-Rane <mohitsrane16@gmail.com>
2022-06-08 17:43:27 +0530

    Creating & Validating User

--- src/environments/prod.env.ts
+++ src/environments/prod.env.ts
@@ -1,5 +1,5 @@
 import {Environment} from './env';
 
 export const ProdEnvironment : Environment= {
-    db_url: 'mongodb+srv://admin:admin@cluster101.eyren.mongodb.net/?retryWrites=true&w=majority'
+    db_url: 'mongodb+srv://test:mongodbuser@mongodbtrainingcluster-80cd4.mongodb.net/test?retryWrites=true&w=majority'
 };
